0de05fcb8c60f60577ad9b5d38f7e601c9b6e727
prslc <nicolmarylyn11761252@gmail.com>
2025-03-29 01:20:29 +0800

    fix (NoActive_Additioanl): Set persist.sys.millet.handshake to true

--- Modules/NoActive_Additional/post-fs-data.sh
+++ Modules/NoActive_Additional/post-fs-data.sh
@@ -4,7 +4,7 @@ if [ "$(getprop persist.sys.gz.enable)" = "true" ]; then
     resetprop -n persist.sys.gz.enable false
     resetprop -n persist.sys.brightmillet.enable false
     resetprop -n persist.sys.powmillet.enable false
-    resetprop -n persist.sys.millet.handshake false
+    resetprop -n persist.sys.millet.handshake true
 fi
 
 # Hans
